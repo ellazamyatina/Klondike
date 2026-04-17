409dd491a1a5b22e25c907f5f9e9ad144dd566d7
Ella Zamyatina <230418248+ellazamyatina@users.noreply.github.com>
2026-04-18 02:11:27 +0300

    Fix

--- src/test/kotlin/model/RankTest.kt
+++ src/test/kotlin/model/RankTest.kt
@@ -2,37 +2,36 @@ package model
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
-class RankTest{
+class RankTest  {
 
     @Test
-    fun `every rank equals its value`(){
-        val ranks: List<Rank> = Rank.entries.sortedBy {it.value}
-        ranks.forEachIndexed {index:Int, rank ->
+    fun `every rank equals its value`()  {
+        val ranks: List<Rank> = Rank.entries.sortedBy { it.value }
+        ranks.forEachIndexed { index: Int, rank ->
             val expectedValue: Int = index + 1
             assertEquals(expectedValue, rank.value)
         }
     }
 
     @Test
-    fun `every rank has its right character`(){
-        Rank.entries.forEach {rank->
-            val expected = when(rank){
-                Rank.ACE->"A"
-                Rank.JACK->"J"
-                Rank.QUEEN->"Q"
-                Rank.KING->"K"
-                else-> rank.value.toString()
-            }
+    fun `every rank has its right character`()  {
+        Rank.entries.forEach { rank ->
+            val expected =
+                when (rank) {
+                    Rank.ACE -> "A"
+                    Rank.JACK -> "J"
+                    Rank.QUEEN -> "Q"
+                    Rank.KING -> "K"
+                    else -> rank.value.toString()
+                }
             assertEquals(expected, rank.displaySymbol())
         }
     }
 
     @Test
-    fun `KING has the highest value and the ACE the lowest value`(){
-        assertEquals(1,Rank.ACE.value)
+    fun `KING has the highest value and the ACE the lowest value`()  {
+        assertEquals(1, Rank.ACE.value)
         assertEquals(13, Rank.KING.value)
     }
 }
