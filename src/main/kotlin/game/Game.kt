e074f12bd7aeb3b18e9ad0b755713ebd41582e53
Ella Zamyatina <230418248+ellazamyatina@users.noreply.github.com>
2026-04-30 21:49:01 +0300

    Implement main logic(Game.kt) and add tests

---
+++ src/main/kotlin/game/Game.kt
@@ -0,0 +1,91 @@
+package game
+
+import model.Card
+import model.Suit
+import model.Move
+import model.createShuffledPile
+
+class Game{
+
+    // make all piles for the game
+
+    val tableau  = List(7) {TablePile()}
+    val foundations = Suit.entries.map{ FoundationPile(it) }
+    val stock = StockPile()
+    val waste = WastePile()
+
+    // move history
+    private val moveHistory = mutableListOf<Move>()
+
+    fun initialize(){
+        val deck = createShuffledPile()
+        var index = 0
+
+
+        // put shuffled cards for 7 table piles
+        for (col in 0..6){
+            repeat(col+1){row->
+                val card = deck[index++]
+
+                // the top card is face up (row == col)
+                tableau[col].addCard(card.copy(isFaceUp = (row == col)))
+            }
+
+        }
+
+        // remainder located in the Stock Pile
+        while(index < deck.size){
+            stock.addCard(deck[index++].copy())
+        }
+    }
+
+    // make move, return true if the move is valid and done
+
+    fun makeMove(move: Move): Boolean{
+
+        // empty move
+        if (move.cards.isEmpty()) return false
+
+        // the target card for move
+        val bottomCard = move.cards.last()
+        if(!move.toPile.canPlace(bottomCard)) return false
+
+        // delete
+        move.cards.forEach {move.fromPile.removeTop()}
+
+        // open new card for tableau
+        flipNewTopIfTableau(move.fromPile)
+
+        // add new card, bottom card should be placed first
+        move.cards.reversed().forEach { move.toPile.addCard(it) }
+
+        // save for undo
+        moveHistory.add(move)
+        return true
+    }
+
+    // undo the last move
+    fun undo(): Boolean {
+        if (moveHistory.isEmpty()) return false
+
+        val lastMove = moveHistory.removeAt(moveHistory.lastIndex)
+
+        lastMove.cards.forEach {lastMove.fromPile.addCard(it)}
+        repeat(lastMove.cards.size) { lastMove.toPile.removeTop()}
+        return true
+    }
+
+    fun isGameWon(): Boolean = foundations.all {it.isComplete()}
+
+    // function for open new cards in the tableau
+    private fun flipNewTopIfTableau(pile:Pile){
+        if (pile is TablePile && !pile.isEmpty()){
+            val top: Card? = pile.topCard()
+            if (top!= null && !top.isFaceUp){
+                pile.removeTop()
+                pile.addCard(top.copy(isFaceUp = true))
+            }
+        }
+    }
+
+}
